32e791abc50bb3af081c23792e7a8573d09255cd
Kiran <kiransarwar05@gmail.com>
2026-06-28 19:23:10 +0500

    Understanding datatypes
    There are 5 datatypes that are mostly used. String, float, none, boolean, and int.

---
+++ 01. Vriables and Datatypes/03_Datatypes.py
@@ -0,0 +1,12 @@
+# My Third Python Program
+# Author: Kiran
+# Date: 28th June, 2026
+# Description: Understanding Data Types in Python
+
+name = "Kiran" # string data type
+age = 20 # integer data type
+height = 5.9 # float data type
+
+print(type(name)) # Output: <class 'str'>
+print(type(age)) # Output: <class 'int'>
+print(type(height)) # Output: <class 'float'>

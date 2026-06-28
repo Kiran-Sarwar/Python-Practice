13935d4dbf7e8d3b2f44e6bb3c6e82d8c5a45853
Kiran <kiransarwar05@gmail.com>
2026-06-28 19:14:57 +0500

    Understanding Variables

---
+++ 01. Vriables and Datatypes/02_Variables.py
@@ -0,0 +1,12 @@
+# My Second Python Program
+# Author: Kiran
+# Date: 28th June, 2026
+# Description: Understanding Variables in Python
+
+name = "Kiran"
+age = 20
+print("My name is ", name)
+print("My age is ", age)
+
+name = "John" #reasigned
+print("My name is ", name)

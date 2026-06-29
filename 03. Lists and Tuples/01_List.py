add0281a2e37b272999f1b55598533c3dc887960
Kiran <kiransarwar05@gmail.com>
2026-06-29 23:03:21 +0500

    Understanding List
    Things I have learned
    01_ List
    02_ List slicing
    03_ List Methods

---
+++ 03. Lists and Tuples/01_List.py
@@ -0,0 +1,15 @@
+# Author = Kiran
+# Date = 29th June, 2026
+# Description = Understaning Lists
+
+marks = [34, 45, 12, 89, 91]
+print(marks)
+print(marks[2])
+print(len(marks))
+
+# List Slicing
+# Ending index is not included
+Student = ["Kiran", 87, "Python"]
+Student[ : 2]
+Student[1 : ]
+Student[-3 : -1]
---
+++ 03. Lists and Tuples/02_List_Methods.py
@@ -0,0 +1,16 @@
+# Author = Kiran
+# Date = 29th June, 2026
+# Description = List methods
+
+list = [1, 2, 3, 4, 5]
+
+list.append(6)
+print(list)
+list.sort()
+print(list)
+list.sort(reverse=True)
+print(list)
+list.reverse()
+print(list)
+list.insert(2, 10)
+print(list)

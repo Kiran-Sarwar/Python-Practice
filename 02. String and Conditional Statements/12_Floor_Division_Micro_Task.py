5862431fa29bca8d8883e4894d8d6bd8f266adaf
Kiran <kiransarwar05@gmail.com>
2026-07-01 15:59:56 +0500

    Floor Division Micro-Task

---
+++ 02. String and Conditional Statements/12_Floor_Division_Micro_Task.py
@@ -0,0 +1,8 @@
+# Author = Kiran
+# Date = 1st July, 2026
+"""
+Floor Division Micro-Task
+Write Python code using the floor division operator // to find how many whole groups of 3 fit into 10.
+"""
+result = 10 // 3
+print(result)

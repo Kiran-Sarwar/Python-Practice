60c5e04b91fb266009f4e7fe5ac3b26bdd1a87d4
Kiran <kiransarwar05@gmail.com>
2026-07-04 22:13:12 +0500

    While loop

---
+++ 05. Loops/01.while.py
@@ -0,0 +1,24 @@
+# Author = Kiran
+# Date = 4th July, 2026
+# Description = This program demonstrates the use of while loop in Python.
+
+count = 1
+while count <= 5:
+    print("Hello")
+    count += 1
+
+
+print(count)
+i = 1
+while i <= 10:
+    print(i)
+    i += 1
+
+print("loop ended")
+
+# another way
+i = 10
+while i > 1:
+    print(i)
+    i -= 1
+ print("loop ended")
